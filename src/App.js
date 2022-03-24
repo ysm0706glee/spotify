4e7a3103bffa87024e45e707d6228325356295e9
ysm0706glee <ysm0706glee@gmail.com>
2022-03-24 14:05:49 +0100

    add random music feature

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [randomMusic, setRamdomMusic] = useState([]);
+  const [randomButtom, setRandomButtom] = useState(false);
+
+  useEffect(() => {
+    const getRandomSongs = async () => {
+      try {
+        const response = await axios.get(
+          "https://gj05ju1755.execute-api.eu-west-1.amazonaws.com/dev/codechallenge/music/random"
+        );
+        setRamdomMusic(response.data.items);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    getRandomSongs();
+  }, [randomButtom]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={() => setRandomButtom(!randomButtom)}>random</button>
+      {randomMusic.length &&
+        randomMusic.map((x) => {
+          return (
+            <div key={x.id}>
+              <img src={x.album.images[2].url} alt={x.album.name} />
+              <span>{x.album.name}</span>
+              <span>{x.artists[0].name}</span>
+              <a
+                rel="noreferrer"
+                href={x.album.external_urls.spotify}
+                target="_blank"
+              >
+                listen
+              </a>
+            </div>
+          );
+        })}
     </div>
   );
-}
+};
 
 export default App;
